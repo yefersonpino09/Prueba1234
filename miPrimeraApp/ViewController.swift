698935be53e308201a3eb1a73b7d0605324f05fd
e <e>
2022-09-12 14:44:58 -0500

    Initial Commit

---
+++ miPrimeraApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  miPrimeraApp
+//
+//  Created by Mac 02 on 12/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
